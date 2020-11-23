7ef32829c3a1e148362a701a6c3890dc3cbdfa75
Grimpher <komarnicki115@gmail.com>
2020-11-23 12:33:41 +0100

    save 3rd chapter solutions

---
+++ 3.Operatory/script.js
@@ -0,0 +1,43 @@
+// Zadanie 1
+let prawda = true;
+let falsz = false;
+console.log(prawda === falsz);
+
+
+// Zadanie 2
+let pierwszaLiczba = 47;
+let drugaLiczba = 22;
+let moduloResult = 0;
+
+moduloResult = 47 % 22;
+console.log(moduloResult);
+
+// Zadanie 3
+let pierwszyString = "Hello ";
+let drugiString = "World";
+let stringsResult = "";
+stringsResult = pierwszyString + drugiString;
+
+console.log(stringsResult);
+
+// Zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString);
+console.log(someNumber === someString);
+
+// Zadanie 5
+let counter = 30;
+console.log(counter);
+
+console.log(++counter);
+console.log(--counter);
+
+// Zadanie 6
+let pierwszaZmienna = 24;
+let drugaZmienna = 16;
+let result = null;
+
+result = pierwszaZmienna > drugaZmienna;
+console.log(result);
