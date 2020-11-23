17ccb3e4bf965fc759d1556486741f79e7007b98
Grimpher <komarnicki115@gmail.com>
2020-11-23 12:23:34 +0100

    add lab 2 solutions

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,26 @@
+// Zadanie 1
+let number = 47;
+let string = "Hello world";
+let mixed = 47 + " Lorem";
+let logic = false;
+let notNumber = NaN;
+
+console.log(number);
+console.log(string);
+console.log(mixed);
+console.log(logic);
+console.log(notNumber);
+
+// Zadanie 2
+let firstNumber = 10;
+let secondNumber = 22;
+let sum = 0;
+
+sum = firstNumber + secondNumber;
+console.log("Wynik sumy: " + sum);
+
+// Zadanie 3
+let unknown;
+console.log(unknown);
+// Javascript zwraca wartość undefined, ponieważ zmienna została
+// zadeklarowana ale nie przypisano do niej wartości
